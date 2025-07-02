1685ea31b445f17098e6db3dfdf27273927da8a2
parkminjo <cruiser367@naver.com>
2025-07-02 12:17:49 +0900

    style: TodoItem에 styled-components 적용 및 레이아웃 개선

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import type { Todo } from '../../types/todo.type';
 
 interface Props {
@@ -10,18 +11,56 @@ const TodoItem = ({ todo, updateTodo, deleteTodo }: Props) => {
   const { id, content, isCompleted } = todo;
 
   return (
-    <li style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-      <input
-        type="checkbox"
-        checked={isCompleted}
-        onChange={() => updateTodo(id)}
-      />
-      <p style={{ textDecoration: isCompleted ? 'line-through' : 'none' }}>
-        {content}
-      </p>
-      <button onClick={() => deleteTodo(id)}>삭제</button>
-    </li>
+    <TodoItemWrapper>
+      <TodoItemText $isCompleted={isCompleted}>{content}</TodoItemText>
+      <TodoItemActions>
+        <ActionButton
+          $bgColor={isCompleted ? '#242424' : '#582be7'}
+          onClick={() => updateTodo(id)}
+        >
+          {isCompleted ? '취소하기' : '완료하기'}
+        </ActionButton>
+        <ActionButton $bgColor="#ff4033" onClick={() => deleteTodo(id)}>
+          삭제하기
+        </ActionButton>
+      </TodoItemActions>
+    </TodoItemWrapper>
   );
 };
+
+const TodoItemWrapper = styled.li`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: white;
+  padding: 1.25rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const TodoItemText = styled.p<{ $isCompleted: boolean }>`
+  text-decoration: ${({ $isCompleted }) =>
+    $isCompleted ? 'line-through' : 'none'};
+`;
+
+const TodoItemActions = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+`;
+
+const ActionButton = styled.button<{ $bgColor?: string }>`
+  background-color: ${({ $bgColor = '#e6582b' }) => $bgColor};
+  color: white;
+  border: none;
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
 
 export default TodoItem;
