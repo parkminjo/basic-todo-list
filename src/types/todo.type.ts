0efeb08906bf46d0e13024b47b30080849120863
parkminjo <cruiser367@naver.com>
2025-07-04 20:24:28 +0900

    refactor: Todo 타입을 Supabase 테이블 타입으로 변경 및 불필요한 인터페이스 제거

--- src/types/todo.type.ts
+++ src/types/todo.type.ts
@@ -1,10 +1,4 @@
 import type { Tables } from "./supabase"
 
-type SupabaseTodo = Tables<'todo_list'>
+export type Todo = Tables<'todo_list'>
 
-export interface Todo {
-  id: SupabaseTodo['id']
-  createdAt: SupabaseTodo['created_at']
-  content: SupabaseTodo['content']
-  completed: SupabaseTodo['completed']
-}
