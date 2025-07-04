858a5be7727701ac24fbcc5283e8d3890e0034d5
parkminjo <cruiser367@naver.com>
2025-07-04 20:24:41 +0900

    refactor: TodoProvider에서 Supabase를 사용하여 할 일 목록을 가져오도록 수정

--- src/components/provider/TodoProvider.tsx
+++ src/components/provider/TodoProvider.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/supabase';
 import { TodoContext } from '../../context/TodoContext';
 import { TODO_STATUS } from '../../constants/todo';
+import { TODO_LIST_TABLE } from '../../constants/supabaseTable';
 import type { Todo } from '../../types/todo.type';
 
 interface Props {
@@ -9,22 +11,32 @@ interface Props {
 
 const TodoProvider = ({ children }: Props) => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const addTodo = (content: Todo['content']) => {
-    setTodoList((prev) => [
-      {
-        id: crypto.randomUUID(),
-        content,
-        isCompleted: false,
-      },
-      ...prev,
-    ]);
+  const getTodoList = async () => {
+    try {
+      const { data, error } = await supabase.from(TODO_LIST_TABLE).select();
+
+      if (error) throw new Error(error.message);
+
+      setTodoList(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const addTodo = async (content: Todo['content']) => {
+    await supabase
+      .from(TODO_LIST_TABLE)
+      .insert({ content, is_completed: false });
   };
 
   const updateTodo = (id: Todo['id']) => {
     setTodoList((prev) =>
       prev.map((todo) =>
-        todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+        todo.id === id ? { ...todo, isCompleted: !todo.completed } : todo
       )
     );
   };
@@ -36,13 +48,17 @@ const TodoProvider = ({ children }: Props) => {
   const getFilteredTodoList = (selectedFilter?: string | null) => {
     switch (selectedFilter) {
       case TODO_STATUS.COMPLETED:
-        return todoList.filter((todo) => todo.isCompleted);
+        return todoList.filter((todo) => todo.completed);
       case TODO_STATUS.PENDING:
-        return todoList.filter((todo) => !todo.isCompleted);
+        return todoList.filter((todo) => !todo.completed);
       default:
         return todoList;
     }
   };
+
+  useEffect(() => {
+    getTodoList();
+  }, []);
 
   const value = {
     data: {
@@ -50,6 +66,7 @@ const TodoProvider = ({ children }: Props) => {
       completed: getFilteredTodoList(TODO_STATUS.COMPLETED),
       pending: getFilteredTodoList(TODO_STATUS.PENDING),
     },
+    isLoading,
     addTodo,
     updateTodo,
     deleteTodo,
