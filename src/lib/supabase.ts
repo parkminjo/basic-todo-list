c8f2b150308c400b9661508d2d9abbdf22115cb2
parkminjo <cruiser367@naver.com>
2025-07-04 19:53:25 +0900

    init: Supabase 설정 파일 및 타입 정의 추가

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+import type { Database } from '../types/supabase';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY as string;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_API_KEY as string;
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY);
