1f15646a7f4d7a9e2c883c7982c997a508485282
parkminjo <cruiser367@naver.com>
2025-07-04 22:58:56 +0900

    feat: 할 일 목록 로딩 상태 추가

--- src/components/provider/TodoProvider.tsx
+++ src/components/provider/TodoProvider.tsx
@@ -11,6 +11,7 @@ interface Props {
 
 const TodoProvider = ({ children }: Props) => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getTodoList = async () => {
     try {
@@ -19,6 +20,8 @@ const TodoProvider = ({ children }: Props) => {
     } catch (error) {
       console.error('할 일 목록 불러오기 실패', error);
       alert('할 일 목록을 불러오는데 실패하였습니다.');
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -84,6 +87,7 @@ const TodoProvider = ({ children }: Props) => {
       completed: getFilteredTodoList(TODO_STATUS.COMPLETED),
       pending: getFilteredTodoList(TODO_STATUS.PENDING),
     },
+    isLoading,
     addTodo,
     updateTodo,
     deleteTodo,
--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -3,6 +3,7 @@ import type { Todo } from '../types/todo.type';
 
 interface TodoContextType {
   data: Record<string, Todo[]>;
+  isLoading: boolean;
   addTodo: (content: Todo['content']) => void;
   updateTodo: (id: Todo['id'], currentCompleted: Todo['completed']) => void;
   deleteTodo: (id: Todo['id']) => void;
