0c69d07f6da79e66fe1c1b456b4955f7e5d4611f
parkminjo <cruiser367@naver.com>
2025-07-04 22:41:24 +0900

    feat: axios로 할 일 목록을 불러오는 기능 추가

--- src/components/provider/TodoProvider.tsx
+++ src/components/provider/TodoProvider.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { TodoContext } from '../../context/TodoContext';
 import { TODO_STATUS } from '../../constants/todo';
 import type { Todo } from '../../types/todo.type';
+import { todoClient } from '../../lib/todoClient';
 
 interface Props {
   children: React.ReactNode;
@@ -9,6 +10,16 @@ interface Props {
 
 const TodoProvider = ({ children }: Props) => {
   const [todoList, setTodoList] = useState<Todo[]>([]);
+
+  const getTodoList = async () => {
+    try {
+      const { data } = await todoClient.get('/');
+      setTodoList(data);
+    } catch (error) {
+      console.error('할 일 목록 불러오기 실패', error);
+      alert('할 일 목록을 불러오는데 실패하였습니다.');
+    }
+  };
 
   const addTodo = (content: Todo['content']) => {
     setTodoList((prev) => [
@@ -43,6 +54,10 @@ const TodoProvider = ({ children }: Props) => {
         return todoList;
     }
   };
+
+  useEffect(() => {
+    getTodoList();
+  }, []);
 
   const value = {
     data: {
