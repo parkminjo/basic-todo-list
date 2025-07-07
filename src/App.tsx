08e9357728f7d7c9d35d410fdf4325a6686929f7
parkminjo <cruiser367@naver.com>
2025-07-07 12:52:50 +0900

    refactor: TodoProvider 제거 및 todoApi 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import { PATH } from './constants/path';
 import RootLayout from './components/layout/RootLayout';
 import HomePage from './pages/HomePage';
 import TodoDetailPage from './pages/TodoDetailPage';
-import TodoProvider from './components/provider/TodoProvider';
 import QueryProvider from './components/provider/QueryProvider';
 
 const App = () => {
   return (
     <QueryProvider>
-      <TodoProvider>
-        <Routes>
-          <Route path={PATH.HOME} element={<RootLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path={PATH.TODO_DETAIL} element={<TodoDetailPage />} />
-          </Route>
-        </Routes>
-      </TodoProvider>
+      <Routes>
+        <Route path={PATH.HOME} element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path={PATH.TODO_DETAIL} element={<TodoDetailPage />} />
+        </Route>
+      </Routes>
     </QueryProvider>
   );
 };
---
+++ src/api/todoApi.ts
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { TODO_API } from "../constants/apiPath";
+import type { Todo } from "../types/todo.type";
+
+
+export const todoClient = axios.create({
+	baseURL:'http://localhost:4000/todoList',
+})
+
+export const getTodo = async (id: Todo['id']) => {
+		try {
+			const { data }: { data: Todo } = await todoClient.get(TODO_API.BY_ID(id));
+			return data;
+		} catch (error) {
+			console.error('할 일 불러오기 실패', error);
+			throw error;
+		}
+	};
+
+	export const getTodoList = async () => {
+		try {
+			const { data }: { data: Todo[] } =  await todoClient.get(TODO_API.ROOT);
+			return data;
+		} catch (error) {
+			console.error('할 일 목록 불러오기 실패', error);
+			alert('할 일 목록을 불러오는데 실패하였습니다.');
+		} 
+	};
+
+export const addTodo = async (content: Todo['content']) => {
+		try {
+			await todoClient.post(TODO_API.ROOT, {
+				content,
+				completed: false,
+			});
+
+		} catch (error) {
+			console.error('할 일 목록 추가 실패', error);
+			alert('할 일을 추가하는데 실패하였습니다.');
+		}
+	};
+
+export const updateTodo = async (
+		id: Todo['id'],
+		currentCompleted: Todo['completed']
+	) => {
+		try {
+			await todoClient.patch(TODO_API.BY_ID(id), {
+				completed: !currentCompleted,
+			});
+
+		} catch (error) {
+			console.error('할 일 수정 실패', error);
+			alert('할 일을 수정하는데 실패하였습니다.');
+		}
+	};
+
+export const deleteTodo = async (id: Todo['id']) => {
+		try {
+			await todoClient.delete(TODO_API.BY_ID(id));
+
+		} catch (error) {
+			console.error('할 일 삭제 실패', error);
+			alert('할 일을 삭제하는데 실패하였습니다.');
+		}
+	};
