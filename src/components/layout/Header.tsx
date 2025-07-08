4f014ea3c0880a2f6dae5d2016e1c5ffa991f6ff
parkminjo <cruiser367@naver.com>
2025-07-08 13:48:43 +0900

    refactor: Header에 tailwind css 적용

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,33 +1,16 @@
-import styled from 'styled-components';
 import { PATH } from '../../constants/path';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <HeaderContainer>
-      <HeaderContent>
-        <HeaderTitle to={PATH.HOME}>TODO</HeaderTitle>
-      </HeaderContent>
-    </HeaderContainer>
+    <header className="h-[50px]">
+      <div className="flex flex-row items-center max-w-3xl mx-auto p-4">
+        <Link to={PATH.HOME} className="text-xl font-bold">
+          TODO
+        </Link>
+      </div>
+    </header>
   );
 };
-
-const HeaderContainer = styled.header`
-  height: 50px;
-`;
-
-const HeaderContent = styled.div`
-  display: flex;
-  align-items: center;
-  max-width: 768px;
-  height: 100%;
-  margin: 0 auto;
-  padding: 1.5rem;
-`;
-
-const HeaderTitle = styled(Link)`
-  font-size: 1.25rem;
-  font-weight: bold;
-`;
 
 export default Header;
