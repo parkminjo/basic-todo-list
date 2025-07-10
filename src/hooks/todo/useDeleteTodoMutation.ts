6f06db822051acf6c1bd2b73e515ff4979f61019
parkminjo <cruiser367@naver.com>
2025-07-10 18:16:58 +0900

    feat: 투두 삭제할 때 toast 알림 추가

--- src/hooks/todo/useDeleteTodoMutation.ts
+++ src/hooks/todo/useDeleteTodoMutation.ts
@@ -1,12 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { deleteTodo } from "../../api/todoApi"
 import { QUERY_KEY } from "../../constants/queryKey"
+import { useToastStore } from "../../store/useToastStore";
 
 export const useDeleteTodoMutation = () => {
-	const queryClient = useQueryClient()
+	const queryClient = useQueryClient();
+		const { addToast } = useToastStore();
+	
 
 	return useMutation({
     mutationFn: deleteTodo,
-		onSuccess: () => queryClient.invalidateQueries({queryKey: [QUERY_KEY.TODO_LIST]})
+		onSuccess: () => {
+			addToast('투두가 성공적으로 삭제되었습니다.')
+		},
+		onSettled: () => queryClient.invalidateQueries({queryKey: [QUERY_KEY.TODO_LIST]})
   })
 }
