e517bcae62e70cfc45f3d5b1d9a957cff7f20a7a
parkminjo <cruiser367@naver.com>
2025-07-10 18:17:14 +0900

    feat: 투두 완료 상태 변경할 때 toast 알림 추가

--- src/hooks/todo/useUpdateTodoMutation.ts
+++ src/hooks/todo/useUpdateTodoMutation.ts
@@ -1,12 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { updateTodo } from "../../api/todoApi"
 import { QUERY_KEY } from "../../constants/queryKey"
+import { useToastStore } from "../../store/useToastStore";
 
 export const useUpdateTodoMutation = () => {
-	const queryClient = useQueryClient()
+	const queryClient = useQueryClient();
+	const { addToast } = useToastStore();
+	
 
 	return useMutation({
 		mutationFn: updateTodo,
-		onSuccess: () => queryClient.invalidateQueries({queryKey: [QUERY_KEY.TODO_LIST]})
+		onSuccess: () => {
+			addToast('투두 완료 상태가 성공적으로 변경되었습니다.')
+		},
+		onSettled: () => queryClient.invalidateQueries({queryKey: [QUERY_KEY.TODO_LIST]})
 	})
 }
