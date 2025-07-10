ea80685ce4356fc708516d56188cd5dd468c4b21
parkminjo <cruiser367@naver.com>
2025-07-10 18:24:13 +0900

    style: Toaster margin bottom 및 text-align-center 추가

--- src/components/Toaster.tsx
+++ src/components/Toaster.tsx
@@ -7,7 +7,10 @@ const Toaster = () => {
     <ul className="fixed top-4 left-1/2 z-40 -translate-x-1/2">
       {toasts.map((toast) => {
         return (
-          <li key={toast.id} className="bg-white rounded-lg shadow-md p-4">
+          <li
+            key={toast.id}
+            className="bg-white rounded-lg shadow-md p-4 mb-3 text-center"
+          >
             {toast.message}
           </li>
         );
