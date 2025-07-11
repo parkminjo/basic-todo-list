822375117357985f328c77ff5500a8a71685b9f6
parkminjo <cruiser367@naver.com>
2025-07-11 13:54:16 +0900

    refactor: 에러 및 정보 메시지 상수화

---
+++ src/constants/errorMessage.ts
@@ -0,0 +1,6 @@
+export const TODO_ERROR_MESSAGE = {
+	GET_TODO_LIST: '할 일 목록을 불러오는 중 오류가 발생했습니다.',
+	POST_TODO: '할 일을 추가하는 중 오류가 발생했습니다.',
+	PATCH_TODO: '할 일을 수정하는 중 오류가 발생했습니다.',
+	DELETE_TODO: '할 일을 삭제하는 중 오류가 발생했습니다.',
+}
---
+++ src/constants/infoMessage.ts
@@ -0,0 +1,5 @@
+export const TODO_INFO_MESSAGE = {
+	POST_TODO: '할 일을 추가했습니다.',
+	PATCH_TODO: '할 일 완료 상태가 변경되었습니다.',
+	DELETE_TODO: '할 일을 삭제했습니다.',
+};
